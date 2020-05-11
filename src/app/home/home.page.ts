3bf685eab14856c0d6db14334aabb9c77731aee0
MinhPhuPham <phamminhphueur@gmail.com>
2020-05-11 18:44:46 +0700

    Intia commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from '../cart.service';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
-
+export class HomePage implements OnInit {
+ 
+  cart = [];
+  items = [];
+ 
+  sliderConfig = {
+    slidesPerView: 1.6,
+    // initialSlide: 1,
+    speed: 400,
+    spaceBetween: 10,
+    centeredSlides: true
+  };
+ 
+  constructor(private router: Router, private cartService: CartService) { }
+ 
+  ngOnInit() {
+    this.items = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+  }
+ 
+  addToCart(product) {
+    this.cartService.addProduct(product);
+  }
+ 
+  openCart() {
+    this.router.navigate(['cart']);
+  }
 }
